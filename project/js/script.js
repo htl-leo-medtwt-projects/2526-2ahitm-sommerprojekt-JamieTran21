5122a513ee9c01a5aa7ce6223669ebb39de24f02
Jamie <j.tran1@students.htl-leonding.ac.at>
2026-04-05 19:03:02 +0200

    Character Lineup und Team Level Box gemacht

--- project/js/script.js
+++ project/js/script.js
@@ -1,17 +1,34 @@
 let overlay = document.getElementById("overlay");
+let Character1Name = "Aemeath";
+let Character2Name = "Castorice";
+let Character3Name = "Qiuyuan";
+let TeamLevelNumber = 3;
 
 
 function navigateToLobby(){
    
     overlay.innerHTML = `<div id="lobbyScreen">
-    <div id="WorldSelecter">
-      <div id="PracticeRange"><p>Practice Range</p></div>
-        <div id="worldIcon">
-            <img src="./img/World_The_Xianzhou_Luofu.png" alt="worldIcon">
+        <div id="navbar">
+            
         </div>
-        <div id="ConfirmButton"><p>Confirm Planet</p></div>
-    </div>
-    
+        <div id="WorldSelecter">
+        <div id="PracticeRange"><p>Practice Range</p></div>
+            <div id="worldIcon">
+                <img src="./img/World_The_Xianzhou_Luofu.png" alt="worldIcon">
+            </div>
+            <div id="ConfirmButton"><p>Confirm Planet</p></div>
+        </div>
+        <div id="CharactersLineup">
+            <div id="Character1"><div id="Character1NameBox"><p>${Character1Name}</p></div></div>
+            <div id="Character2"><div id="Character2NameBox"><p>${Character2Name}</p></div></div>
+            <div id="Character3"><div id="Character3NameBox"><p>${Character3Name}</p></div></div>
+        </div>
+        <div id="TeamLevel">
+            <p>Team Level</p>
+            <p id="TeamLevelNumber">${TeamLevelNumber}</p>
+
+        </div>
+        
     
     
     
