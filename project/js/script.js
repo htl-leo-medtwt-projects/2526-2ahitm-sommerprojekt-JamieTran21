4ef28c6b5fbc10c054f6eddb3f27b2df146a8ed7
Jamie <j.tran1@students.htl-leonding.ac.at>
2026-04-05 18:05:48 +0200

    box für planeten auswahl und übungsbereich gemacht

--- project/js/script.js
+++ project/js/script.js
@@ -2,7 +2,22 @@ let overlay = document.getElementById("overlay");
 
 
 function navigateToLobby(){
-    overlay.innerHTML = ""
+   
+    overlay.innerHTML = `<div id="lobbyScreen">
+    <div id="WorldSelecter">
+      <div id="PracticeRange"><p>Practice Range</p></div>
+        <div id="worldIcon">
+            <img src="./img/World_The_Xianzhou_Luofu.png" alt="worldIcon">
+        </div>
+        <div id="ConfirmButton"><p>Confirm Planet</p></div>
+    </div>
+    
+    
+    
+    
+    </div>`
+    overlay.style.backgroundImage = "url('./img/lobbyScreen.png')"
+
 
 
 
